2f2dce9ef40045eb263ae70d452bc528e176d371
L&H <abc@hub.moe>
2024-04-08 10:35:38 +0800

    feat: testing scope package in monorepo+typescript

--- apps/react-demo/src/component/mobx/MobxThemeSwitch.tsx
+++ apps/react-demo/src/component/mobx/MobxThemeSwitch.tsx
@@ -2,6 +2,10 @@ import { observer } from 'mobx-react-lite';
 import { useEffect } from 'react';
 import { useThemeStore } from '../../store/global/theme.store';
 
+// Testing typescript type-check in Monorepo
+// Scope package(@types/css-modules) in this project
+import styles from '../../style/switch.module.less';
+
 export const MobxThemeSwitch = observer(function MobxThemeSwitch() {
     const themeStore = useThemeStore();
 
@@ -11,7 +15,9 @@ export const MobxThemeSwitch = observer(function MobxThemeSwitch() {
 
     return (
         <p>
-            <button onClick={() => themeStore.toggle()}>Switch Theme (Global Level)</button>
+            <button className={styles.button} onClick={() => themeStore.toggle()}>
+                Switch Theme (Global Level)
+            </button>
         </p>
     );
 });
